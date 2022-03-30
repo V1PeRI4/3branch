513c14cc196de234964828055aecb7649a915836
user <user@C1014>
2022-03-30 12:35:29 +0300

    никита 2

--- 3branch/Program.cs
+++ 3branch/Program.cs
@@ -1,4 +1,6 @@
-﻿//Комент от некита
+﻿Console.WriteLine("Это написал Никита");
+Console.WriteLine("Это написал Никита 2");
+//Комент от некита
 
 Console.WriteLine("Это написал Тим");
 Console.WriteLine("Это написал Никита");
